c3134fa3977929090c67a192855fa50aca3e2527
Pabitra Dakua <pabitradakua@Pabitras-MacBook-Air.local>
2026-05-22 15:08:09 +0530

    fix

--- src/modules/organisation/organization.module.ts
+++ src/modules/organisation/organization.module.ts
@@ -4,11 +4,12 @@
 import {Module} from "@nestjs/common"
 import { OrganisationService } from "./organization.service";
 import { ProximityModule } from "../psearch/psearch.module";
+import { OrganizationController } from "./organization.controller";
 
 
 @Module({
   imports: [ProximityModule],
-  controllers: [],
+  controllers: [OrganizationController],
   providers: [OrganisationService],
   exports: [OrganisationService],
 })
